b691d76cb51c045ed227d8d9924d37c218235232
thomlink <59086534+thomlink@users.noreply.github.com>
2020-02-04 12:37:37 +0000

    Did another looping tast
    Task 3
    Count the numbers between 1 and 1000 that are divisible by 3 with no remainder using each loop.

--- hello-world/src/main/scala/tomlinker/Looping.scala
+++ hello-world/src/main/scala/tomlinker/Looping.scala
@@ -3,38 +3,44 @@ package com.tomlinker
 
 // Lots of impurity ew
 object looping extends App {
-	whileLoop()
-	doWhileLoop()
-	forLoop1
-	forLoop2
+	println(whileLoop())
+	println(doWhileLoop())
+	// forLoop1
+	println(forLoop2)
 
 
-	def whileLoop(){
+	def whileLoop() : Int = {
 		var i = 1
-		while (i < 10){
-			println(i)
-			i = i + 2
+		var count = 0
+		while (i < 1000){
+			if (i % 3 == 0) count = count + 1
+			i = i + 1
 		}
+		count
 	}
 
-	def doWhileLoop(){
+	def doWhileLoop() : Int = {
 		var i = 1
+		var count = 0
 		do {
- 			println(i)
- 			i = i + 2
-		} while(i < 10)
+ 			if (i % 3 == 0) count = count + 1
+ 			i = i + 1
+		} while(i < 1000)
+		count
 	}
 
-	def forLoop1(){
-		for (i <- Array(1, 3, 5, 7, 9)) {
-			println(i)
-		}
-	}
+	// def forLoop1(){
+	// 	for (i <- Array(1, 3, 5, 7, 9)) {
+	// 		println(i)
+	// 	}
+	// }
 
-	def forLoop2(){
-		for (i <- (1 to 10)) {
-			if (i % 2 != 0) println(i)
+	def forLoop2() : Int = {
+		var count = 0
+		for (i <- (1 to 1000)) {
+			if (i % 3 == 0) count = count + 1
 		}
+		count
 		
 	}
 
