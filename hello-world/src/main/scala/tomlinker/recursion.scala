99fcad7a8c6b2a510e731fd4fe1547059a7ca117
thomlink <59086534+thomlink@users.noreply.github.com>
2020-02-04 14:43:58 +0000

    Completed different loops exercise
    Task 2
    Print a count from 1 to 10 in increments of 2: 1,3,5,7,9 using appropriate  loops.

--- hello-world/src/main/scala/tomlinker/recursion.scala
+++ hello-world/src/main/scala/tomlinker/recursion.scala
@@ -5,7 +5,31 @@ import scala.annotation.tailrec
 
 object recursion extends App{
 	
-	println(downToOne(20))
+	//println(downToOne(20))
+
+
+	val nums = (1 to 10 by 2)
+
+	//foreach
+	nums.foreach(num => println(num))
+
+	//map
+	val mapped = nums.map(num => println(num))
+
+	//fold
+	val folded = nums.fold("")(
+	        (acc, num) => acc + num.toString + "\n"
+	    )
+	println(folded)
+
+	//filter
+	(1 to 20).filter(num => 
+	num % 2 != 0 && num <= 10).foreach(println)
+
+
+
+
+
 
 	def downToOne(current: Int) : String = {
 		@tailrec
